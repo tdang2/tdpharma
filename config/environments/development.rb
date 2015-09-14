793feaa247d4ef960d08f92c83d1607ac36d2431
Tri Dang <quangtrister@gmail.com>
2015-09-14 17:43:31 -0400

    trigger build

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,6 +43,5 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.delivery_method = :letter_opener
-
   Paperclip.options[:command_path] = "/usr/local/bin/"
 end

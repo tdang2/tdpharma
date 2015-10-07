944419012f3122520227feec3a9e26f47ed76f2b
Tri Dang <quangtrister@gmail.com>
2015-10-06 21:19:23 -0400

    https://trello.com/c/vcg5zh43 third checklist

--- spec/models/medicine_spec.rb
+++ spec/models/medicine_spec.rb
@@ -6,4 +6,18 @@ RSpec.describe Medicine, type: :model do
   it {should have_many :med_batches}
   it {should have_many :inventory_items}
   it {should have_one :image}
+  describe 'association behavior' do
+    describe 'when deleted' do
+      it 'destroy all related inventory items from all stores' do
+
+      end
+      it 'destroy all related med_batches' do
+
+      end
+      it 'destroy the related image' do
+
+      end
+    end
+  end
+
 end

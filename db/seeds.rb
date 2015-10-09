469a00725ca682228f07f5a45a5765a063c944f0
Robin Dang <dang.robin@gmail.com>
2015-10-08 20:48:21 -0400

    Changed seed login email

--- db/seeds.rb
+++ db/seeds.rb
@@ -78,7 +78,7 @@ Category.create(name: "Mushrooms", parent_id: c6.id)
 
 s = Store.create(name: 'Store')
 s.categories << Category.all
-User.create(first_name: 'Robin', last_name: 'Dang', email: 'dang.robin@gmail.com', password: 'password', store_id: s.id)
+User.create(first_name: 'Robin', last_name: 'Dang', email: 'test@test.com', password: 'password', store_id: s.id)
 User.create(first_name: 'Tri', last_name: 'Dang', email: 'tri@gmail.com', password: 'password', store_id: s.id)
 
 

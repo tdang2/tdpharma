b8126f3179c3c3c63095c91b00a616690bc13c9b
Tri Dang <quangtrister@gmail.com>
2015-12-17 19:30:14 +0700

    reset aws initializer

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,6 +2,6 @@ require 'aws-sdk'
 
 Aws.config.update({region: ENV['S3_REGION'], credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])})
 
-S3_BUCKET = Aws::S3::Resource.new(region: ENV['S3_REGION']).bucket(ENV['S3_BUCKET_NAME'])
-S3_PUBLIC_BUCKET = Aws::S3::Resource.new(region: ENV['S3_REGION']).bucket(ENV['S3_PUBLIC_BUCKET'])
+S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET_NAME'])
+S3_PUBLIC_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_PUBLIC_BUCKET'])
 S3_PUBLIC_CLIENT = Aws::S3::Client.new(region: ENV['S3_REGION'])

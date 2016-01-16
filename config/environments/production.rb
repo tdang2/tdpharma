64e11e13077a0f1a11cc2acb9b7540fa3b6c34fe
Tri Dang <quangtrister@gmail.com>
2016-01-16 12:58:33 +0700

    heroku aws set up configuration update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,14 +89,4 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: ENV["ASSET_HOST"] }
 
   config.assets.precompile += %w[active_admin.css active_admin.js]
-
-  config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_region => ENV['S3_REGION'],
-      :s3_credentials => {
-          :bucket => ENV['S3_BUCKET_NAME'],
-          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
-  }
 end
